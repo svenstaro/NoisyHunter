2cc60c26a43ea7a7ce9938109a67aca0d79e8886
opatut <opatutlol@aol.com>
2010-09-04 23:09:32 +0200

    Added Coordinates struct.

---
+++ src/engine/Input.hpp
@@ -0,0 +1,14 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+
+// Structure holding screen and game coordinates
+
+struct Coordinates{
+public:
+    float X, Y;
+    int ScreenX, ScreenY;
+};
+
+
+#endif
