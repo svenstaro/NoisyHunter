8735d49ff29dce80ab23bb44d69d5ade108d8b44
opatut <opatutlol@aol.com>
2010-09-21 21:50:20 +0200

    Server Mainstate added.

--- game/src/server/main.cpp
+++ game/src/server/main.cpp
@@ -1,8 +1,11 @@
 #include "Root.hpp"
 
+#include "MainState.hpp"
+
 int main(){
     Engine::Root& root = Engine::Root::get_mutable_instance();
     root.InitializeAsServer(12356);
+    root.GetStateManagerPtr()->Add(new MainState());
     root.StartMainLoop();
     return 0;
 }
