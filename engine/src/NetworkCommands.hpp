28476c5fb32da02c145ed1657494e178d3a1efba
Sven-Hendrik Haase <sh@lutzhaase.com>
2010-09-25 17:31:47 +0200

    Changing network command values.

--- engine/src/NetworkCommands.hpp
+++ engine/src/NetworkCommands.hpp
@@ -5,7 +5,7 @@ namespace Engine {
 
 enum NetworkCommand {
 	// If this is sent client->server, the client is requesting being added
-	// to the server's list of known clients.
+	// to the server's list of known clients. Basically, this is a handshake.
 	// If this is sent server->client, the client is being notified of a new
 	// accepted client.
 	NETCMD_CLIENTADD = 0x00,
@@ -19,24 +19,24 @@ enum NetworkCommand {
 	// added.
 	// If this is sent server->client, the client is being notified of a new 
 	// entity.
-	NETCMD_ENTITYADD = 0x02,
+	NETCMD_ENTITYADD = 0x10,
 
 	// If this is sent client->server, a client is requesting an entity to 
 	// start an action. An action is defined in a concrete entity's 
 	// EntityAction enum. This could be ENTITYMOVE or ENTITYSHOOT, for example.
 	// If this is sent server->client, the client is being notified of an
 	// entity's action. This is useful for interpolating.
-	NETCMD_ENTITYACTION = 0x03,
+	NETCMD_ENTITYACTION = 0x11,
 
 	// If this is sent client->server, ALL HELL BREAKS LOSE
 	// If this is sent server->client, the client is being notified of the
 	// complete data of an entity. This info needs to be deserialized.
-	NETCMD_ENTITYINFO = 0x04,
+	NETCMD_ENTITYINFO = 0x12,
 
 	// If this is sent client->server, a client is requesting a chat message
 	// to be posted to all other clients.
 	// If this is sent server->client, the client is being sent a chat message.
-	NETCMD_CHATMESSAGE = 0x06
+	NETCMD_CHATMESSAGE = 0x20
 };
 
 }
