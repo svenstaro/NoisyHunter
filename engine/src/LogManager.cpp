b5d112addc41fc3d8f76cb21d266905f9722fb24
Matz Radloff <matzradloff@gmx.de>
2010-10-01 19:05:49 +0200

    Also applied new log system for errors

--- engine/src/LogManager.cpp
+++ engine/src/LogManager.cpp
@@ -29,6 +29,8 @@ void LogManager::Log(enum LogLevel log_level, enum LogOrigin log_origin, const s
 		log_message += "RESOURCEMANAGER] ";
 	else if(log_origin == LOGORIGIN_GUI)
 		log_message += "GUI] ";
+	else if(log_origin == LOGORIGIN_IDMANAGER)
+		log_message += "IDMANAGER] ";
 	
 
 	log_message += NO_COLOR + text;
