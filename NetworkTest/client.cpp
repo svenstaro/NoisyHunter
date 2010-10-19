a01e1cd4522f508c2dbd77d2190ac8fbe7ecbfa1
opatut <opatutlol@aol.com>
2010-10-20 01:47:47 +0200

    Added client side receiving.

--- NetworkTest/client.cpp
+++ NetworkTest/client.cpp
@@ -11,13 +11,13 @@ int main(int argc, char* argv[]){
     }
     
     sf::SocketUDP mSocket;
+    mSocket.Bind(23456);
     sf::IPAddress ip(argv[1]);
     sf::Uint16 port = boost::lexical_cast<sf::Uint16>(argv[2]);
     sf::Packet packet;
     std::string t = "Hello";
     std::string name = argv[3];
     packet << t << name;
-    mSocket.Send(packet, ip, port);
     mSocket.Send(packet, ip, port);
     std::cout << "Hello server, I am " << name << std::endl;
 
@@ -31,6 +31,19 @@ int main(int argc, char* argv[]){
         packet << t;
 
         mSocket.Send(packet, ip, port);
+
+        sf::IPAddress sip;
+        sf::Uint16 sport;
+        mSocket.Receive(packet, sip, sport);
+        sf::Uint16 client_num;
+        packet >> client_num;
+        std::string lol = " Clients: ";
+        for (int i = 0; i < client_num; i++){
+            std::string client_name;
+            packet >> client_name;
+            lol += client_name;
+        }
+        std::cout << client_num << lol << std::endl;
         sf::Sleep(0.5);        
     }
     return 1;
