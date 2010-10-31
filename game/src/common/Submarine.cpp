0269083868935c2e638978dbb49035365a708b4a
Matz Radloff <matzradloff@gmx.de>
2010-11-01 00:17:26 +0100

    Minor changes on particle settings.

--- game/src/common/Submarine.cpp
+++ game/src/common/Submarine.cpp
@@ -29,13 +29,14 @@ void Submarine::Initialize() {
 
 	// Particle system for submarine
 	Engine::Vector2D position = Engine::Vector2D(0.f, 0.f);
-	Engine::Vector2D direction = Engine::Vector2D(0.f, -1.f);
+	Engine::Vector2D direction = Engine::Vector2D(0.f, -1.2f);
 	Engine::ParticleSystem* part_sys = new Engine::ParticleSystem(position, direction, Engine::Entity::PositionType::POSITIONTYPE_WORLD);
 	Engine::ParticleEmitter* part_emit = new Engine::ParticleEmitter(Engine::Vector2D(0.f, 0.f), 0.f, 0.1f, 20.f);
-	part_emit->SetRate(7.f);
+	part_emit->SetRate(1.7f);
 	part_emit->SetTimeToLive(5.f);
 	part_emit->SetStartScale(0.5f);
-	part_emit->SetEndScale(1.f);
+	part_emit->SetEndScale(2.f);
+	part_emit->SetStartAlpha(180);
 	part_emit->SetEndAlpha(0);
 	part_emit->SetImageName("particle_bubble");
 	part_emit->SetEndColor(sf::Color(0,0,255));
